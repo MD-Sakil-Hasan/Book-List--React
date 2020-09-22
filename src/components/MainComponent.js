264b9cc0116228287cc7e707cbf7555f9dabb3b5
MD-Sakil-Hasan <mdsakilhasan833@gmail.com>
2020-09-22 17:31:47 +0600

    Almost Done

---
+++ src/components/MainComponent.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import bookList from "../assets/books";
+import BookList from "./lists/BookList";
+import NewBook from "./represantational/NewBook";
+import { Route, NavLink, Switch, Redirect } from "react-router-dom";
+import BookDetail from "./represantational/BookDetail";
+
+class MainComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: bookList,
+      selectedBook: null
+    };
+  }
+
+  selectedBookHandler = bookId => {
+    const book = this.state.books.filter(book =>
+      book.id === bookId )[0];
+    this.setState({
+      selectedBook: book
+    })
+  }
+
+  render() {
+    const books = (
+      <BookList
+        books={this.state.books}
+        selectedBookHandler={this.selectedBookHandler}
+      />
+    );
+
+    return (
+      <div className="App">
+        <nav className="nav-bar">
+          <ul>
+            <li>
+              <NavLink to="/" exact>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/new-book">New Book</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/books" exact render={() => books} />
+          <Route path="/new-book" exact component={NewBook} />
+          <Route path="/:id" render={() => <BookDetail book={this.state.selectedBook} />} />
+          <Redirect from="/" to="/books" />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default MainComponent;
